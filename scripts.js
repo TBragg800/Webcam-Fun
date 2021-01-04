847a494d210d46016261a87311c565c050af0bb2
LM&T <tbbragg83@gmail.com>
2021-01-04 01:43:01 -0600

    added function to take photo and add red effect/append photos to photobooth strip

--- scripts.js
+++ scripts.js
@@ -22,9 +22,42 @@ function paintToCanvas() {
   canvas.width = width;
   canvas.height = height;
 
-  setInterval(() => {
+  return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+
+    //take pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    //mess with them
+    pixels = redEffect(pixels);
+
+    //put them back
+    ctx.putImageData(pixels, 0, 0);
+
   }, 16);
 }
 
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "handsome");
+  link.innerHTML = `<img src="${data}" alt="hansome man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; //red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
+  }
+  return pixels;
+}
+
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
